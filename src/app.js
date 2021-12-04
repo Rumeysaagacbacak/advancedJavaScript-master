adcd3912fd3f681ca36419b761df1791d652ee94
rumeysa <rumey@DESKTOP-M5AHE8G>
2021-12-04 22:30:37 +0300

    18 Kasım 2021 ders sonu

---
+++ src/app.js
@@ -0,0 +1,35 @@
+console.log("Merhaba kodlama.io")
+//js type safe değildir.
+ let dolarBugun = 9.30
+ let dolarDun =  9.20
+{
+    let dolarDun=9.10
+}
+ console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 bunu yapamıyoruz .
+console.log(euroDun)
+
+//array[]
+//PascalCasing ikiside büyük
+//camelCasing ilk harf büyük sonraki küçük
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi","Özel konut kredisi"]
+console.log("<ul>")
+for (let i = 0; i <konutKredileri.length; i++) 
+{
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+console.log(konutKredileri)
+
+
+
+
+
+
+
+
+ 
